ad05b1c0e656b071b6ce3bcf6f0eb1a798065c91
leon.liu <twappleon@gmail.com>
2025-06-26 10:46:33 +0800

    Enhance EthereumClient implementation by adding ContractManager interface methods for deploying and interacting with smart contracts, including ERC20 token balance and transfer functionalities. Update dependencies in go.mod and go.sum for new libraries.

--- client/ethereum.go
+++ client/ethereum.go
@@ -2,26 +2,32 @@ package client
 
 import (
 	"context"
+	"encoding/hex"
 	"errors"
 	"math/big"
+	"strings"
 
 	"github.com/blockchain-sdk-go/api/types"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-// EthereumClient 實作 BlockchainClient, WalletManager, TokenManager
-// 你可以根據實際需求擴充欄位
+// EthereumClient 實作 BlockchainClient, WalletManager, TokenManager, ContractManager
 
 type EthereumClient struct {
 	rpcURL string
 	client *ethclient.Client
 }
 
-// 确保 EthereumClient 实现 BlockchainClient, WalletManager, TokenManager
 var _ types.BlockchainClient = (*EthereumClient)(nil)
 var _ types.WalletManager = (*EthereumClient)(nil)
 var _ types.TokenManager = (*EthereumClient)(nil)
+var _ types.ContractManager = (*EthereumClient)(nil)
 
 // Connect 實作 BlockchainClient 介面
 func (e *EthereumClient) Connect(ctx context.Context, url string) error {
@@ -36,9 +42,14 @@ func (e *EthereumClient) Connect(ctx context.Context, url string) error {
 
 // GenerateNewWallet 實作 WalletManager 介面
 func (e *EthereumClient) GenerateNewWallet() (privateKey string, address string, err error) {
-	// 这里建议用 go-ethereum 的 crypto 包生成新私钥
-	// 这里只返回假数据，实际可用 crypto.GenerateKey()
-	return "dummy_eth_private_key", "dummy_eth_address", nil
+	key, err := crypto.GenerateKey()
+	if err != nil {
+		return "", "", err
+	}
+	privBytes := crypto.FromECDSA(key)
+	privateKey = hex.EncodeToString(privBytes)
+	address = crypto.PubkeyToAddress(key.PublicKey).Hex()
+	return privateKey, address, nil
 }
 
 // GetNativeBalance 实现 TokenManager
@@ -51,22 +62,191 @@ func (e *EthereumClient) GetNativeBalance(ctx context.Context, address string) (
 	if err != nil {
 		return nil, err
 	}
-	// ETH 单位转换：Wei -> Ether
 	ethValue := new(big.Float).Quo(new(big.Float).SetInt(balance), big.NewFloat(1e18))
 	return ethValue, nil
 }
 
 // SendNativeToken 实现 TokenManager
 func (e *EthereumClient) SendNativeToken(ctx context.Context, fromPrivateKey, toAddress string, amount *big.Float) (string, error) {
-	// 这里建议用 go-ethereum 的 bind 包和 crypto 包实现真实转账
-	// 这里只返回假数据，实际实现较复杂
-	return "dummy_tx_hash", nil
+	if e.client == nil {
+		return "", errors.New("Ethereum client not connected")
+	}
+	priv, err := crypto.HexToECDSA(fromPrivateKey)
+	if err != nil {
+		return "", err
+	}
+	fromAddr := crypto.PubkeyToAddress(priv.PublicKey)
+	nonce, err := e.client.PendingNonceAt(ctx, fromAddr)
+	if err != nil {
+		return "", err
+	}
+	valueWei := new(big.Int)
+	amountWei := new(big.Float).Mul(amount, big.NewFloat(1e18))
+	amountWei.Int(valueWei)
+	gasLimit := uint64(21000)
+	gasPrice, err := e.client.SuggestGasPrice(ctx)
+	if err != nil {
+		return "", err
+	}
+	to := common.HexToAddress(toAddress)
+	tx := ethtypes.NewTransaction(nonce, to, valueWei, gasLimit, gasPrice, nil)
+	chainID, err := e.client.NetworkID(ctx)
+	if err != nil {
+		return "", err
+	}
+	signedTx, err := ethtypes.SignTx(tx, ethtypes.NewEIP155Signer(chainID), priv)
+	if err != nil {
+		return "", err
+	}
+	err = e.client.SendTransaction(ctx, signedTx)
+	if err != nil {
+		return "", err
+	}
+	return signedTx.Hash().Hex(), nil
 }
+
+// DeployContract 实现 ContractManager
+func (e *EthereumClient) DeployContract(ctx context.Context, privateKey, bytecode, abiJSON string, constructorArgs []interface{}) (string, error) {
+	if e.client == nil {
+		return "", errors.New("Ethereum client not connected")
+	}
+	priv, err := crypto.HexToECDSA(privateKey)
+	if err != nil {
+		return "", err
+	}
+	fromAddr := crypto.PubkeyToAddress(priv.PublicKey)
+	nonce, err := e.client.PendingNonceAt(ctx, fromAddr)
+	if err != nil {
+		return "", err
+	}
+	gasPrice, err := e.client.SuggestGasPrice(ctx)
+	if err != nil {
+		return "", err
+	}
+	parsedABI, err := abi.JSON(strings.NewReader(abiJSON))
+	if err != nil {
+		return "", err
+	}
+	bytecodeBytes, err := hex.DecodeString(strings.TrimPrefix(bytecode, "0x"))
+	if err != nil {
+		return "", err
+	}
+	auth, err := bind.NewKeyedTransactorWithChainID(priv, big.NewInt(1)) // 1=mainnet, 可根据实际链ID调整
+	if err != nil {
+		return "", err
+	}
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)
+	auth.GasLimit = uint64(3000000)
+	auth.GasPrice = gasPrice
+	address, _, _, err := bind.DeployContract(auth, parsedABI, bytecodeBytes, e.client, constructorArgs...)
+	if err != nil {
+		return "", err
+	}
+	return address.Hex(), nil
+}
+
+// CallContract 实现 ContractManager
+func (e *EthereumClient) CallContract(ctx context.Context, contractAddress, abiJSON, method string, params []interface{}) (interface{}, error) {
+	if e.client == nil {
+		return nil, errors.New("Ethereum client not connected")
+	}
+	parsedABI, err := abi.JSON(strings.NewReader(abiJSON))
+	if err != nil {
+		return nil, err
+	}
+	contract := common.HexToAddress(contractAddress)
+	callData, err := parsedABI.Pack(method, params...)
+	if err != nil {
+		return nil, err
+	}
+	msg := ethereum.CallMsg{
+		To:   &contract,
+		Data: callData,
+	}
+	output, err := e.client.CallContract(ctx, msg, nil)
+	if err != nil {
+		return nil, err
+	}
+	var result interface{}
+	if err := parsedABI.UnpackIntoInterface(&result, method, output); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+// ERC20 余额查询
+func (e *EthereumClient) GetERC20Balance(ctx context.Context, contractAddress, walletAddress string) (*big.Int, error) {
+	parsedABI, err := abi.JSON(strings.NewReader(ERC20ABI))
+	if err != nil {
+		return nil, err
+	}
+	contract := common.HexToAddress(contractAddress)
+	data, err := parsedABI.Pack("balanceOf", common.HexToAddress(walletAddress))
+	if err != nil {
+		return nil, err
+	}
+	msg := ethereum.CallMsg{
+		To:   &contract,
+		Data: data,
+	}
+	output, err := e.client.CallContract(ctx, msg, nil)
+	if err != nil {
+		return nil, err
+	}
+	return new(big.Int).SetBytes(output), nil
+}
+
+// ERC20 转账
+func (e *EthereumClient) TransferERC20(ctx context.Context, privateKey, contractAddress, toAddress string, amount *big.Int) (string, error) {
+	priv, err := crypto.HexToECDSA(privateKey)
+	if err != nil {
+		return "", err
+	}
+	fromAddr := crypto.PubkeyToAddress(priv.PublicKey)
+	parsedABI, err := abi.JSON(strings.NewReader(ERC20ABI))
+	if err != nil {
+		return "", err
+	}
+	data, err := parsedABI.Pack("transfer", common.HexToAddress(toAddress), amount)
+	if err != nil {
+		return "", err
+	}
+	nonce, err := e.client.PendingNonceAt(ctx, fromAddr)
+	if err != nil {
+		return "", err
+	}
+	gasPrice, err := e.client.SuggestGasPrice(ctx)
+	if err != nil {
+		return "", err
+	}
+	contract := common.HexToAddress(contractAddress)
+	tx := ethtypes.NewTransaction(nonce, contract, big.NewInt(0), uint64(60000), gasPrice, data)
+	chainID, err := e.client.NetworkID(ctx)
+	if err != nil {
+		return "", err
+	}
+	signedTx, err := ethtypes.SignTx(tx, ethtypes.NewEIP155Signer(chainID), priv)
+	if err != nil {
+		return "", err
+	}
+	err = e.client.SendTransaction(ctx, signedTx)
+	if err != nil {
+		return "", err
+	}
+	return signedTx.Hash().Hex(), nil
+}
+
+// ERC20 ABI 常量
+const ERC20ABI = `[
+  {"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"type":"function"},
+  {"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"type":"function"}
+]`
 
 // Close 實作 BlockchainClient 介面
 func (e *EthereumClient) Close() error {
 	if e.client != nil {
-		return e.client.Close()
+		e.client.Close()
 	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -14,8 +14,10 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/bits-and-blooms/bitset v1.20.0 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
@@ -23,12 +25,15 @@ require (
 	github.com/consensys/gnark-crypto v0.16.0 // indirect
 	github.com/crate-crypto/go-eth-kzg v1.3.0 // indirect
 	github.com/crate-crypto/go-ipa v0.0.0-20240724233137-53bbb0ceb27a // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/ethereum/c-kzg-4844/v2 v2.1.0 // indirect
 	github.com/ethereum/go-ethereum v1.15.11 // indirect
 	github.com/ethereum/go-verkle v0.2.2 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/spec v0.20.4 // indirect
@@ -38,6 +43,8 @@ require (
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/holiman/uint256 v1.3.2 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -49,8 +56,11 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/supranational/blst v0.3.14 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
