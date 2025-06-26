8357c3b1fe37521e9b8c199834c1fd34a919e622
leon.liu <twappleon@gmail.com>
2025-06-26 10:59:55 +0800

    Update Dockerfile to use Go 1.24, switch from Alpine to Debian-based image for package installation, and enhance TronClient implementation with TokenManager and ContractManager interfaces, including methods for wallet generation, balance retrieval, and token transfers. Add new dependencies in go.mod and go.sum for gotron-sdk and Ethereum libraries.

--- client/tron.go
+++ client/tron.go
@@ -2,35 +2,128 @@ package client
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"math/big"
 
 	"github.com/blockchain-sdk-go/api/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/fbsobreira/gotron-sdk/pkg/address"
+	"github.com/fbsobreira/gotron-sdk/pkg/client"
+	"github.com/fbsobreira/gotron-sdk/pkg/proto/core"
+	"github.com/golang/protobuf/proto"
 )
 
-// TronClient 實作 WalletManager 介面
-// 你可以根據實際需求擴充欄位
-
-// 確保 TronClient 實作 BlockchainClient, WalletManager
-var _ types.BlockchainClient = (*TronClient)(nil)
-var _ types.WalletManager = (*TronClient)(nil)
+// TronClient 實作 BlockchainClient, WalletManager, TokenManager, ContractManager
 
 type TronClient struct {
-	// 可擴充欄位
+	nodeURL string
+	client  *client.GrpcClient
 }
+
+var _ types.BlockchainClient = (*TronClient)(nil)
+var _ types.WalletManager = (*TronClient)(nil)
+var _ types.TokenManager = (*TronClient)(nil)
+var _ types.ContractManager = (*TronClient)(nil)
 
 // Connect 實作 BlockchainClient 介面
 func (t *TronClient) Connect(ctx context.Context, url string) error {
-	// 這裡寫連線邏輯，暫時回傳 nil
+	cli := client.NewGrpcClient(url)
+	err := cli.Start()
+	if err != nil {
+		return err
+	}
+	t.nodeURL = url
+	t.client = cli
 	return nil
 }
 
 // GenerateNewWallet 實作 WalletManager 介面
-func (t *TronClient) GenerateNewWallet() (privateKey string, address string, err error) {
-	// 這裡寫產生錢包邏輯，這裡先回傳假資料
-	return "dummy_private_key", "dummy_address", nil
+func (t *TronClient) GenerateNewWallet() (privateKey string, addr string, err error) {
+	key, err := crypto.GenerateKey()
+	if err != nil {
+		return "", "", err
+	}
+	privBytes := crypto.FromECDSA(key)
+	tronAddr := address.PubkeyToAddress(key.PublicKey).String()
+	return hex.EncodeToString(privBytes), tronAddr, nil
+}
+
+// GetNativeBalance 实现 TokenManager
+func (t *TronClient) GetNativeBalance(ctx context.Context, addr string) (*big.Float, error) {
+	if t.client == nil {
+		return nil, errors.New("Tron client not connected")
+	}
+	tronAddr, err := address.Base58ToAddress(addr)
+	if err != nil {
+		return nil, err
+	}
+	acc, err := t.client.GetAccount(tronAddr.String())
+	if err != nil {
+		return nil, err
+	}
+	balance := new(big.Float).SetInt(big.NewInt(acc.Balance))
+	return balance, nil
+}
+
+// SendNativeToken 实现 TokenManager
+func (t *TronClient) SendNativeToken(ctx context.Context, fromPrivateKey, toAddress string, amount *big.Float) (string, error) {
+	if t.client == nil {
+		return "", errors.New("Tron client not connected")
+	}
+	priv, err := crypto.HexToECDSA(fromPrivateKey)
+	if err != nil {
+		return "", err
+	}
+	fromAddr := address.PubkeyToAddress(priv.PublicKey).String()
+	amt := big.NewInt(0)
+	amount.Int(amt)
+	txn, err := t.client.Transfer(fromAddr, toAddress, amt.Int64())
+	if err != nil {
+		return "", err
+	}
+	_, err = t.client.Broadcast(txn.Transaction)
+	if err != nil {
+		return "", err
+	}
+	txid := getTronTxID(txn.Transaction)
+	return txid, nil
+}
+
+// DeployContract 实现 ContractManager
+func (t *TronClient) DeployContract(ctx context.Context, privateKey, bytecode, abiJSON string, constructorArgs []interface{}) (string, error) {
+	// gotron-sdk 暂无直接合约部署API，需用 TriggerSmartContract 创建合约
+	return "", errors.New("Tron contract deployment not implemented in gotron-sdk, please use TronBox or TronGrid")
+}
+
+// CallContract 实现 ContractManager
+func (t *TronClient) CallContract(ctx context.Context, contractAddress, abiJSON, method string, params []interface{}) (interface{}, error) {
+	// gotron-sdk 支持 TriggerConstantContract 调用合约方法
+	return nil, errors.New("Tron contract call not implemented in this demo, see gotron-sdk TriggerConstantContract")
+}
+
+// TRC20 余额查询
+func (t *TronClient) GetTRC20Balance(ctx context.Context, contractAddress, walletAddress string) (*big.Int, error) {
+	// 需用 TriggerConstantContract 调用 balanceOf
+	return nil, errors.New("TRC20 balanceOf not implemented in this demo")
+}
+
+// TRC20 转账
+func (t *TronClient) TransferTRC20(ctx context.Context, privateKey, contractAddress, toAddress string, amount *big.Int) (string, error) {
+	// 需用 TriggerSmartContract 调用 transfer
+	return "", errors.New("TRC20 transfer not implemented in this demo")
 }
 
 // Close 實作 BlockchainClient 介面
 func (t *TronClient) Close() error {
-	// 如有資源釋放邏輯可寫於此，暫時回傳 nil
+	// gotron-sdk 没有 Close 方法，留空
 	return nil
+}
+
+// getTronTxID 计算 Tron 交易哈希（TxID）
+func getTronTxID(tx *core.Transaction) string {
+	raw, _ := proto.Marshal(tx.GetRawData())
+	hash := sha256.Sum256(raw)
+	return hex.EncodeToString(hash[:])
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.23.0
 toolchain go1.24.3
 
 require (
+	github.com/ethereum/go-ethereum v1.15.11
+	github.com/fbsobreira/gotron-sdk v0.24.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/joho/godotenv v1.5.1
 	github.com/swaggo/files v1.0.1
@@ -19,16 +21,17 @@ require (
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/bits-and-blooms/bitset v1.20.0 // indirect
+	github.com/btcsuite/btcd v0.22.0-beta // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/consensys/bavard v0.1.27 // indirect
 	github.com/consensys/gnark-crypto v0.16.0 // indirect
 	github.com/crate-crypto/go-eth-kzg v1.3.0 // indirect
 	github.com/crate-crypto/go-ipa v0.0.0-20240724233137-53bbb0ceb27a // indirect
+	github.com/deckarep/golang-set v1.7.1 // indirect
 	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/ethereum/c-kzg-4844/v2 v2.1.0 // indirect
-	github.com/ethereum/go-ethereum v1.15.11 // indirect
 	github.com/ethereum/go-verkle v0.2.2 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
@@ -42,7 +45,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.4 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/holiman/uint256 v1.3.2 // indirect
@@ -52,17 +55,25 @@ require (
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pborman/uuid v1.2.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rjeczalik/notify v0.9.2 // indirect
+	github.com/shengdoushi/base58 v1.0.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/supranational/blst v0.3.14 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/tyler-smith/go-bip39 v1.0.2 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	go.uber.org/atomic v1.6.0 // indirect
+	go.uber.org/multierr v1.5.0 // indirect
+	go.uber.org/zap v1.15.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.35.0 // indirect
 	golang.org/x/net v0.36.0 // indirect
@@ -70,6 +81,8 @@ require (
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	golang.org/x/tools v0.29.0 // indirect
+	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987 // indirect
+	google.golang.org/grpc v1.37.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
